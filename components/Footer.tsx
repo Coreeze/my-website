e43e6b795645e38dec4d4368c490063118a4a304
Coreeze <cristianlenta@yahoo.com>
2022-04-11 15:40:17 +0200

    small fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Grid from './Grid';
 import Link from './Link';
 import Text from './Text';
 import React from 'react';
-import { SiGithub, SiLinkedin, SiTwitter, SiTiktok } from 'react-icons/si';
+import { SiGithub, SiTwitter, SiTiktok } from 'react-icons/si';
 import { MdMail } from 'react-icons/md';
 import { Malt, Instagram } from '@icons';
 
